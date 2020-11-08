a4dd65599347d94a776c462d7598c856447e3071
miacro <fqguozhou@gmail.com>
2020-11-08 15:53:26 +0800

    ...

--- leetcode/regular_expression_matching.py
+++ leetcode/regular_expression_matching.py
@@ -1,38 +1,37 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if len(s) == 0:
-            return all(s[i : i + 2] == ".*" for i in range(len(s) // 2))
-        if len(p) == 0:
-            return False
         table = {(-1, -1): True}
-        for i in range(len(s)):
-            table[(i, -1)] = False
-        for i, p_char in enumerate(p):
-            table[(-1, i)] = table[(-1, i - 1)] and p_char == "*"
-        for s_index in range(len(s)):
-            s_char = s[s_index]
-            for p_index in range(len(p)):
-                p_char = p[p_index]
-                if "a" <= p_char <= "z":
-                    matched = s_char == p_char and table[(s_index - 1, p_index - 1)]
-                elif p_char == ".":
-                    matched = table[(s_index - 1, p_index - 1)]
-                elif p_char == "*":
-                    p_prefix = p[p_index - 1 : p_index]
-                    if p_prefix != "." and p_prefix != s_char:
-                        matched = table[(s_index - 1), (p_index - 1)]
-                    else:
-                        matched = (
-                            table[(s_index - 1, p_index - 1)]
-                            or table[(s_index - 1, p_index)]
-                            or table[(s_index, p_index - 1)]
+        for p_index in range(len(p)):
+            p_char = p[p_index]
+            p_next = p[p_index + 1 : p_index + 2]
+            if p_next == "*":
+                table[(p_index + 1, -1)] = table.get((p_index - 1, -1), False)
+            for s_index in range(len(s)):
+                s_char = s[s_index]
+                matched = False
+                if p_next == "*":
+                    if table.get((p_index - 1, s_index), False):
+                        matched = True
+                    elif p_char == "." or p_char == s_char:
+                        matched = table.get((p_index, s_index - 1), False) or table.get(
+                            (p_index - 1, s_index - 1), False
                         )
-                else:
-                    assert False
-                table[(s_index, p_index)] = matched
-        return table[(len(s) - 1, len(p) - 1)]
+                elif p_char == "*":
+                    matched = table.get((p_index - 2, s_index), False) or table.get(
+                        (p_index - 1, s_index), False
+                    )
+                elif p_char == "." or p_char == s_char:
+                    matched = table.get((p_index - 1, s_index - 1), False)
+                table[(p_index, s_index)] = matched
+
+        return table.get((len(p) - 1, len(s) - 1), False)
 
 
 if __name__ == "__main__":
     solution = Solution()
     assert solution.isMatch("aab", "c*a*b") is True
+    assert solution.isMatch("aa", "a") is False
+    assert solution.isMatch("mississippi", "mis*is*ip*.") is True
+    assert solution.isMatch("abcd", "d*") is False
+    assert solution.isMatch("", ".") is False
+    assert solution.isMatch("", "c*c*") is True
