34cab916c8202f1ac0d3d8046cdc3f42ca6fea3e
miacro <fqguozhou@gmail.com>
2020-11-08 16:15:49 +0800

    ...

--- leetcode/regular_expression_matching.py
+++ leetcode/regular_expression_matching.py
@@ -1,33 +1,32 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        table = {(-1, -1): True}
+        dp = [[False] * (len(s) + 1) for _ in range(len(p) + 1)]
+        dp[-1][-1] = True
         for p_index in range(len(p)):
             p_char = p[p_index]
             if p_index == len(p) - 1:
                 p_next = ""
             else:
                 p_next = p[p_index + 1]
-            if p_next == "*":
-                table[(p_index + 1, -1)] = table.get((p_index - 1, -1), False)
+                if p_next == "*":
+                    dp[p_index + 1][-1] = dp[p_index - 1][-1]
             for s_index in range(len(s)):
                 s_char = s[s_index]
                 matched = False
                 if p_next == "*":
-                    if table.get((p_index - 1, s_index), False):
+                    if dp[p_index - 1][s_index]:
                         matched = True
                     elif p_char == "." or p_char == s_char:
-                        matched = table.get((p_index, s_index - 1), False) or table.get(
-                            (p_index - 1, s_index - 1), False
+                        matched = (
+                            dp[p_index][s_index - 1] or dp[p_index - 1][s_index - 1]
                         )
                 elif p_char == "*":
-                    matched = table.get((p_index - 2, s_index), False) or table.get(
-                        (p_index - 1, s_index), False
-                    )
+                    matched = dp[p_index - 2][s_index] or dp[p_index - 1][s_index]
                 elif p_char == "." or p_char == s_char:
-                    matched = table.get((p_index - 1, s_index - 1), False)
-                table[(p_index, s_index)] = matched
+                    matched = dp[p_index - 1][s_index - 1]
+                dp[p_index][s_index] = matched
 
-        return table.get((len(p) - 1, len(s) - 1), False)
+        return dp[len(p) - 1][len(s) - 1]
 
 
 if __name__ == "__main__":
